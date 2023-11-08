c301e9f73023bec6e8f0d57e57be364c4301c6ca
Chirag Goel <chiragoel3@gmail.com>
2023-11-08 18:15:37 +0000

    Added training code for cross validation of logistic regression classifier

---
+++ models/models_main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+from models.logistic_regression import MultiClassLogisticRegression
+
+def cv_logistic_regression(X_train, y_train, X_val, y_val):
+    num_epochs_list = list(range(1000,10001,1000))
+    hyperparam_list = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.5, 0.9]
+
+    best_acc=0.0
+    best_num_epochs, best_lr, best_lambda1, best_lambda2 = 0.0, 0.0, 0.0, 0.0
+    for  epoch in num_epochs_list:
+        for learning_rate in hyperparam_list:
+            for lambda1 in hyperparam_list:
+                for lambda2 in hyperparam_list:
+                    clf = MultiClassLogisticRegression(
+                        is_weighted=True, 
+                        learning_rate=learning_rate, 
+                        lambda1=lambda1, 
+                        lambda2=lambda2, 
+                        num_epochs=epoch
+                        )
+                    W, b = clf.fit(X_train.T, y_train, X_val.T, y_val)
+                    acc, loss = clf.evaluate_model(X_val.T, y_val, W, b)
+                    if acc>best_acc:
+                        best_num_epochs, best_lr, best_lambda1, best_lambda2 = epoch, learning_rate, lambda1, lambda2
+                        best_acc=acc
--- train.py
+++ train.py
@@ -5,8 +5,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from utils import *
-from models.logistic_regression import MultiClassLogisticRegression 
+from models.logistic_regression import MultiClassLogisticRegression
+from models.models_main import  cv_logistic_regression
 
+def train_logistic_regression(df_train, feats, training_hyperparams):
+    X,y = get_data(df_train, feats, model_name=model_name, is_test=False)
+    X = normalise(X)
+
+    hyperparams = training_hyperparams
+    if training_hyperparams['is_cv']:
+        X_train, X_val = split_dataset(X)
+        y_train, y_val = split_dataset(y)
+        hyperparams['learning_rate'], hyperparams['lambda1'], hyperparams['lambda2'], hyperparams['num_epochs']  = cv_logistic_regression(X_train, y_train, X_val, y_val)
+    
+    clf = MultiClassLogisticRegression(
+        is_weighted=hyperparams['is_weighted_loss'], 
+        learning_rate=hyperparams['learning_rate'], 
+        lambda1=hyperparams['lambda1'], 
+        lambda2=hyperparams['lambda2'], 
+        num_epochs=hyperparams['num_epochs']
+        )
+    best_W, best_b = clf.fit(X.T, y, X.T, y)
 if __name__ == '__main__':
 
     with open('./config.yaml', 'r') as stream:
@@ -24,15 +43,4 @@ if __name__ == '__main__':
     feats = [x for x in df_train.columns if x not in ['Label', 'lat', 'lon','time']]
     
     if model_name=='logistic_regression':
-        X,y = get_data(df_train, feats, model_name=model_name, is_test=False)
-        X = normalise(X)
-
-        hyperparams = config['training']['logistic_regression']
-        clf = MultiClassLogisticRegression(
-            is_weighted=hyperparams['is_weighted_loss'], 
-            learning_rate=hyperparams['learning_rate'], 
-            lambda1=hyperparams['lambda1'], 
-            lambda2=hyperparams['lambda2'], 
-            num_epochs=hyperparams['num_epochs']
-            )
-        best_W, best_b = clf.fit(X.T, y, X.T, y)
+        train_logistic_regression(df_train, feats, config['training']['logistic_regression'])
